451d7d6bd94efe457a8a916c04c35af257000241
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-12-31 11:41:08 +0600

    feat: added script combiner to make single script from parts

---
+++ script_combiner.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+
+def combine_scripts(folder_path: str) -> dict:
+    combined_scripts = dict()
+    files = os.listdir(folder_path)
+    for file in files:
+        if file.endswith(".json"):
+            with open(os.path.join(folder_path, file), "r") as f:
+                data = dict(json.load(f))
+                for (surah_number, surah) in data.items():
+                    for (ayah_number, ayah) in surah.items():
+                        if (surah_number not in combined_scripts):
+                            combined_scripts[surah_number] = dict()
+                        if (ayah_number not in combined_scripts[surah_number]):
+                            combined_scripts[surah_number][ayah_number] = list(
+                            )
+                        combined_scripts[surah_number][ayah_number].append(
+                            ayah)
+
+    return combined_scripts
+
+
+combined_scripts = combine_scripts("quran_parts/indopak_script")
+with open("indopak_with_tajweed/indopak_with_tajweed.json", "w") as f:
+    json.dump(combined_scripts, f, indent=4, ensure_ascii=False)
--- simplifier.py
+++ simplifier.py
@@ -10,25 +10,30 @@ with open("resources_from_qul/indopak.json") as f:
     indopak_script = dict(json.load(f))
 
 
-def simplify(script:dict) -> dict:
+def simplify(script: dict) -> dict:
     simplified = dict()
     for (key, value) in script.items():
         surahNumber, ayahNumber, wordNumber = str(key).split(":")
         text = value["text"]
-        
+
         if surahNumber not in simplified:
             simplified[surahNumber] = dict()
-        
+
         if ayahNumber not in simplified[surahNumber]:
             simplified[surahNumber][ayahNumber] = list()
-        
+
         simplified[surahNumber][ayahNumber].append(text)
-    
+
     return simplified
 
 
 with open("simplified_script/simplified_qpc_hafs_tajweed.json", "w") as f:
-    json.dump(simplify(qpc_hafs_tajweed_script), f, indent=4, ensure_ascii=False)
+    json.dump(
+        simplify(qpc_hafs_tajweed_script),
+        f,
+        indent=4,
+        ensure_ascii=False,
+    )
 
 with open("simplified_script/simplified_indopak_script.json", "w") as f:
     json.dump(simplify(indopak_script), f, indent=4, ensure_ascii=False)
