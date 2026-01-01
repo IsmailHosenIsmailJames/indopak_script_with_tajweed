e88584321c1c1f7e908f931028b8bef05c28ed4e
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2026-01-01 06:40:41 +0600

    feat: will be sorted as ayah key should be

--- script_combiner.py
+++ script_combiner.py
@@ -19,9 +19,14 @@ def combine_scripts(folder_path: str) -> dict:
                         combined_scripts[surah_number][ayah_number].extend(
                             ayah)
 
+                combined_scripts[surah_number] = dict(
+                    sorted(combined_scripts[surah_number].items(),
+                           key=lambda x: int(x[0])))
+
     return combined_scripts
 
 
 combined_scripts = combine_scripts("quran_parts/indopak_script")
+combined_scripts = dict(sorted(combined_scripts.items(), key=lambda x: x[0]))
 with open("indopak_with_tajweed/indopak_with_tajweed.json", "w") as f:
     json.dump(combined_scripts, f, indent=4, ensure_ascii=False)
